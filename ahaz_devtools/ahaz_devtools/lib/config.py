5f07e19702035e69dfadc0b9262c3836cbf11f91
towergame <tower@0x10.lv>
2026-05-26 13:43:51 +0300

    Clean up config.py

--- ahaz_devtools/ahaz_devtools/lib/config.py
+++ ahaz_devtools/ahaz_devtools/lib/config.py
@@ -1,4 +1,2 @@
+# Constant values that might get referenced across multiple files
 REGISTRY_NAME = "ahaz-local-registry"
-REGISTRY_PORT = 6767
-
-REGISTRY_DIR = f"/etc/containerd/certs.d/localhost:{REGISTRY_PORT}"
