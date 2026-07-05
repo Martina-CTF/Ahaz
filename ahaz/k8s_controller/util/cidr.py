a84dbf0f503f30f94c703467fb198c0b1a1033f1
towergame <tower@0x10.lv>
2026-07-05 23:38:16 +0300

    Add explanatory comments for util functions

--- ahaz/k8s_controller/util/cidr.py
+++ ahaz/k8s_controller/util/cidr.py
@@ -2,11 +2,17 @@
 
 
 def cidr_to_netmask(cidr: int) -> str:
+    """
+    Convert a CIDR notation (e.g. 24) to a netmask (e.g. 255.255.255.0).
+    """
     mask = (0xFFFFFFFF >> (32 - cidr)) << (32 - cidr)
     return f"{(mask >> 24) & 0xFF}.{(mask >> 16) & 0xFF}.{(mask >> 8) & 0xFF}.{mask & 0xFF}"
 
 
 def ip_and_cidr_to_netmask(ip_cidr: str) -> str:
+    """
+    Convert an IP address in CIDR notation an IP address with a netmask.
+    """
     ip, cidr = ip_cidr.split("/")
     cidr = int(cidr)
     netmask = cidr_to_netmask(cidr)
@@ -14,6 +20,9 @@ def ip_and_cidr_to_netmask(ip_cidr: str) -> str:
 
 
 def parse_ip_range(ip_range: str) -> str:
+    """
+    Catch-all parser to ensure an IP is in the format of IP and netmask.
+    """
     if ip_range.count("/") == 1:
         return ip_and_cidr_to_netmask(ip_range)
     elif ip_range.count(" ") == 1:
--- ahaz/k8s_controller/util/container.py
+++ ahaz/k8s_controller/util/container.py
@@ -4,6 +4,9 @@ from ahaz_common.task import ImageInformation
 
 
 def get_image_name(image: ImageInformation) -> str:
+    """
+    Returns the full, canonical name of the image based on the information in the ImageInformation object.
+    """
     if image.registry:
         return f"{image.registry}/{image.name}"
     return image.name
